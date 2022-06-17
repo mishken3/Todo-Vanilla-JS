98b22752a3dfc8fb8b6da789aaeb68710686b468
Mikhail <mishken3@gmail.com>
2022-06-17 18:32:45 +0300

    changed tasks text

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 'use strict';
 
 const tasks = [
-  {
-    id: '1138465078061',
-    completed: false,
-    text: 'React',
-  },
-  {
-    id: '1138465078062',
-    completed: false,
-    text: 'Redux',
-  },
-  {
-    id: '1138465078063',
-    completed: false,
-    text: 'Немного отдохнуть...',
-  },
+	{
+		id: '1138465078062',
+		completed: false,
+		text: 'Посмотреть Twin Peaks',
+	},
+	{
+		id: '1138465078061',
+		completed: false,
+		text: 'Побегать',
+	},
+	{
+		id: '1138465078063',
+		completed: false,
+		text: 'Выяснить кто же убил Лору Палмер...',
+	},
 ];
 
 const taskList = document.querySelector('.tasks-list');
